f33238ce1fd711a0f8c6295651b7d4b18f2dd221
Denitsa <denitsamiteva03@schoolmath.eu>
2022-03-20 13:16:38 +0200

    Authorization 1

---
+++ ClassLibrary1/JSONModels/EditUserRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Business.JSONModels
+{
+    public class EditUserRequest
+    {
+        [Required]
+        public Guid ID { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
